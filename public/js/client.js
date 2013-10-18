36b001670c6b5d4e59b243554a4e8088be96e5ce
Kerry Gilbert <kerry@punkave.com>
2013-10-18 12:31:30 -0400

    cool ui stuff

--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
-  
+  var workerCount;
 
 	function getStatus(){
 		$.ajax({
 		  url: "/status",
 		}).done(function(data) {
 		  $('#status').html(data);
+      workerCount = (parseInt($('#worker-count').text())) / (parseInt($('#max-workers').text()) ) * 100+'%';
+      $('.status-bar.workers .value').css('height', workerCount);
+      $('.in-queue').text($('#queue-count').text()); 
+
 		  setTimeout(getStatus,1000);
 		});
 	}
 
 	getStatus();
-
 });
