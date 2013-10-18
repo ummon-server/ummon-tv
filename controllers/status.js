128b3d0085d43d89423beb0cda1fe8545c05e78b
Kerry Gilbert <kerry@punkave.com>
2013-10-18 12:33:15 -0400

    proper merge

--- controllers/status.js
+++ controllers/status.js
@@ -2,10 +2,6 @@ var config = require('../config');
 var restify = require('restify');
 var url = require('url');
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 760588c5ed7badefeae4ee55b5dcb9d11cc3cf8d
 var options = config.server;
 var api = restify.createJsonClient(options);
 if (options.username && options.password) {
