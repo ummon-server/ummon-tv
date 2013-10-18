760588c5ed7badefeae4ee55b5dcb9d11cc3cf8d
Gabriel Farrell <g@grrawr.com>
2013-10-18 12:25:12 -0400

    Moved options and api out of controller function.
    Memory still grows over time.

--- controllers/status.js
+++ controllers/status.js
@@ -3,14 +3,13 @@ var restify = require('restify');
 var url = require('url');
 
 
+var options = config.server;
+var api = restify.createJsonClient(options);
+if (options.username && options.password) {
+  api.basicAuth(options.username, options.password);
+}
 
 module.exports = function(req, res) {
-  var options = config.server;
-  var api = restify.createJsonClient(options);
-  if (options.username && options.password) {
-    api.basicAuth(options.username, options.password);
-  }
-
   api.get('/status', function(err, req, rez, result) {
     res.render('status.html', { 
       result: result, 
